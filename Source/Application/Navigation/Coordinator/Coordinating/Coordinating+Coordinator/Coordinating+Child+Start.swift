91b204afaf64b4af69b2417077fc488123af031e
Sajjon <alex.cyon@gmail.com>
2018-12-02 19:30:20 +0100

    properly removing earlier viewcontrollers from Coordinators NavigationController when using CoordinatorTransition `.replace`. This is using the strong - however likely to be correct - assumption that we always want to clear the navigation stack when starting a coordinator with `replace`

--- Source/Application/Navigation/Coordinator/Coordinating/Coordinating+Coordinator/Coordinating+Child+Start.swift
+++ Source/Application/Navigation/Coordinator/Coordinating/Coordinating+Coordinator/Coordinating+Child+Start.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Open Zesame. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 extension Coordinating {
 
@@ -38,11 +38,13 @@ extension Coordinating {
         // Add the child coordinator to the childCoordinator array
         switch transition {
         case .replace:
-            if !navigationController.viewControllers.isEmpty {
-                navigationController.setViewControllers([], animated: false)
-            }
             childCoordinators = [child]
-        case .append: childCoordinators.append(child)
+            navigationController.removeAllViewControllers { [unowned child] in
+                child.start(didStart: didStart)
+            }
+        case .append:
+            childCoordinators.append(child)
+            child.start(didStart: didStart)
         }
 
         // Subscribe to the navigation steps emitted by the child coordinator
@@ -50,9 +52,24 @@ extension Coordinating {
         bag <~ child.navigator.navigation.do(onNext: {
             navigationHandler($0)
         }).drive()
+    }
+}
 
-        // Start the child coordinator (which is responsible for setting up its root UIViewController and presenting it)
-        // and pass along the `didStart` closure, which the child should invoke or delegate to invoke.
-        child.start(didStart: didStart)
+extension UINavigationController {
+    fileprivate func removeAllViewControllers(animated: Bool = true, completion: @escaping Completion) {
+        func removeAllViewControllers() {
+            if !viewControllers.isEmpty {
+                viewControllers = []
+            }
+            DispatchQueue.main.async { completion() }
+        }
+
+        if let presented = presentedViewController {
+            presented.dismiss(animated: animated) {
+                removeAllViewControllers()
+            }
+        } else {
+            removeAllViewControllers()
+        }
     }
 }
