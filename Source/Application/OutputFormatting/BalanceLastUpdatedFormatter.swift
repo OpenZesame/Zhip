af4311060118ca6e46243736028597dcbf894522
Sajjon <alex.cyon@gmail.com>
2019-01-13 22:02:33 +0000

    Adding date tool for timeAgo functions localized.

---
+++ Source/Application/OutputFormatting/BalanceLastUpdatedFormatter.swift
@@ -0,0 +1,9 @@
+//
+//  BalanceLastUpdatedFormatter.swift
+//  Zhip
+//
+//  Created by Alexander Cyon on 2019-01-13.
+//  Copyright © 2019 Open Zesame. All rights reserved.
+//
+
+import Foundation
