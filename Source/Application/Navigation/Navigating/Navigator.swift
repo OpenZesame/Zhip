c325c7812f1f45d3c40011418caa73287ece4b1b
Sajjon <alex.cyon@gmail.com>
2019-01-23 22:23:19 +0000

    Changing the FILEHEADER for ALL Files.

--- Source/Application/Navigation/Navigating/Navigator.swift
+++ Source/Application/Navigation/Navigating/Navigator.swift
@@ -1,10 +1,18 @@
-import Foundation
-import RxSwift
-import RxCocoa
-
-/// A type capable of stepping to `NavigationStep`s, passed as a generic.
-///
-/// Conforming to `Tracking`, thus capable of tracking the steps which are
+//
+// Copyright 2019 Open Zesame
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under thexc License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 /// marked as `Trackable`.
 final class Navigator<NavigationStep> {
 
