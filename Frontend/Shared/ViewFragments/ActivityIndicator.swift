574ea1fd2353eeebdf535021d95db0967cc85196
Alexander Cyon <alex.cyon@gmail.com>
2022-02-19 13:40:18 +0100

    clean up of ActivityIndicator

--- Frontend/Shared/ViewFragments/ActivityIndicator.swift
+++ Frontend/Shared/ViewFragments/ActivityIndicator.swift
@@ -2,25 +2,38 @@
 //  ActivityIndicator.swift
 //  Zhip
 //
-//  Created by KitKit@stackoverflow on 2022-02-18.
+//  Originall created by KitKit@stackoverflow on 2019-12-04
 //  Profile: https://stackoverflow.com/users/7155095/kitkit
 //  Source: https://stackoverflow.com/a/59171234/1311272
+//
+//  Modified by Alexander Cyon on 2022-02-18.
 //
 
 import SwiftUI
 
 struct ActivityIndicator: View {
     
-    @State private var isAnimating: Bool = false
+    @Binding var isAnimating: Bool
+    private let shouldStartAnimatingOnAppear: Bool
     
+    init(
+        isAnimating: Binding<Bool> = .constant(true),
+        shouldStartAnimatingOnAppear: Bool = true
+    ) {
+        self._isAnimating = isAnimating
+        self.shouldStartAnimatingOnAppear = shouldStartAnimatingOnAppear
+    }
+}
+
+extension ActivityIndicator {
     var body: some View {
         GeometryReader { (geometry: GeometryProxy) in
             ForEach(0..<5) { index in
                 Group {
                     Circle()
                         .frame(width: geometry.size.width / 5, height: geometry.size.height / 5)
-                        .scaleEffect(calcScale(index: index))
-                        .offset(y: calcYOffset(geometry))
+                        .scaleEffect(calculateScale(index: index))
+                        .offset(y: calculateYOffset(geometry))
                 }.frame(width: geometry.size.width, height: geometry.size.height)
                     .rotationEffect(!isAnimating ? .degrees(0) : .degrees(360))
                     .animation(Animation
@@ -30,15 +43,19 @@ struct ActivityIndicator: View {
         }
         .aspectRatio(1, contentMode: .fit)
         .onAppear {
-            isAnimating = true
+            if shouldStartAnimatingOnAppear {
+                isAnimating = true
+            }
         }
     }
-    
-    func calcScale(index: Int) -> CGFloat {
+}
+
+private extension ActivityIndicator {
+    func calculateScale(index: Int) -> CGFloat {
         (!isAnimating ? 1 - CGFloat(Float(index)) / 5 : 0.2 + CGFloat(index) / 5)
     }
     
-    func calcYOffset(_ geometry: GeometryProxy) -> CGFloat {
+    func calculateYOffset(_ geometry: GeometryProxy) -> CGFloat {
         geometry.size.width / 10 - geometry.size.height / 2
     }
     
