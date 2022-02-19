7187da95472c7b33531e0e137e3e5299a5ff3971
Alexander Cyon <alex.cyon@gmail.com>
2022-02-19 14:09:28 +0100

    Fix ActivityIndicator bug where it did not animate.

--- Frontend/Shared/ViewFragments/ActivityIndicator.swift
+++ Frontend/Shared/ViewFragments/ActivityIndicator.swift
@@ -12,47 +12,55 @@
 import SwiftUI
 
 struct ActivityIndicator: View {
-    
-    @Binding var isAnimating: Bool
-    private let shouldStartAnimatingOnAppear: Bool
-    
-    init(
-        isAnimating: Binding<Bool> = .constant(true),
-        shouldStartAnimatingOnAppear: Bool = true
-    ) {
-        self._isAnimating = isAnimating
-        self.shouldStartAnimatingOnAppear = shouldStartAnimatingOnAppear
-    }
+    @State private var isAnimating: Bool = false
+    init() {}
 }
 
 extension ActivityIndicator {
     var body: some View {
-        GeometryReader { (geometry: GeometryProxy) in
+        GeometryReader { geometry in
             ForEach(0..<5) { index in
                 Group {
                     Circle()
-                        .frame(width: geometry.size.width / 5, height: geometry.size.height / 5)
+                        .frame(
+                            width: geometry.size.width / 5,
+                            height: geometry.size.height / 5
+                        )
                         .scaleEffect(calculateScale(index: index))
                         .offset(y: calculateYOffset(geometry))
-                }.frame(width: geometry.size.width, height: geometry.size.height)
-                    .rotationEffect(!isAnimating ? .degrees(0) : .degrees(360))
-                    .animation(Animation
-                                .timingCurve(0.5, 0.15 + Double(index) / 5, 0.25, 1, duration: 1.5)
-                                .repeatForever(autoreverses: false), value: isAnimating)
+                }.frame(
+                    width: geometry.size.width,
+                    height: geometry.size.height
+                )
+                    .rotationEffect(
+                        isAnimating
+                        ? .degrees(360)
+                        : .degrees(0)
+                    )
+                    .animation(
+                        Animation
+                            .timingCurve(
+                                0.5, 0.15 + Double(index) / 5, 0.25,
+                                1,
+                                duration: 1.5
+                            )
+                            .repeatForever(autoreverses: false),
+                        value: isAnimating
+                    )
             }
         }
         .aspectRatio(1, contentMode: .fit)
         .onAppear {
-            if shouldStartAnimatingOnAppear {
-                isAnimating = true
-            }
+            isAnimating = true
         }
     }
 }
 
 private extension ActivityIndicator {
     func calculateScale(index: Int) -> CGFloat {
-        (!isAnimating ? 1 - CGFloat(Float(index)) / 5 : 0.2 + CGFloat(index) / 5)
+        isAnimating
+        ? 0.2 + CGFloat(index) / 5
+        : 1 - CGFloat(Float(index)) / 5
     }
     
     func calculateYOffset(_ geometry: GeometryProxy) -> CGFloat {
