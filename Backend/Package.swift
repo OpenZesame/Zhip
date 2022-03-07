7af63b379a57a30913823ce7d54eb86b501c9e63
Alexander Cyon <alex.cyon@gmail.com>
2022-03-07 20:29:18 +0100

    Clean up Package.swift and also add TCA package

--- Backend/Package.swift
+++ Backend/Package.swift
@@ -5,101 +5,123 @@ import PackageDescription
 import QuartzCore
 
 struct Dependency {
-    enum Category {
-        case essential
-        case navigation
-        case view
-        case `convenience`
-    }
-    let category: Category
-    let package: Package.Dependency
-    let product: PackageDescription.Target.Dependency
-    let rationale: String
+	enum Category {
+		case essential
+		case architecture
+		case navigation
+		case view
+		case `convenience`
+	}
+	let category: Category
+	let package: Package.Dependency
+	let product: PackageDescription.Target.Dependency
+	let rationale: String
+	
+	struct Alternative: ExpressibleByStringLiteral {
+		init(stringLiteral value: String) {
+			self.init(url: value)
+		}
+		let url: String
+		let abstract: String?
+		init(url: String, abstract: String? = nil) {
+			self.url = url
+			self.abstract = abstract
+		}
+	}
+	
+	let alternatives: [Alternative]
+	init(
+		category: Category,
+		package: Package.Dependency,
+		product: PackageDescription.Target.Dependency,
+		rationale: String,
+		alternatives: [Alternative] = []
+	) {
+		self.category = category
+		self.package = package
+		self.product = product
+		self.rationale = rationale
+		self.alternatives = alternatives
+	}
 }
 extension Array where Element == Dependency {
-    var packageDependencies: [Package.Dependency] { map { $0.package } }
-    var targetDependencies: [PackageDescription.Target.Dependency] { map { $0.product } }
+	var packageDependencies: [Package.Dependency] { map { $0.package } }
+	var targetDependencies: [PackageDescription.Target.Dependency] { map { $0.product } }
 }
 
 private let dependencies: [Dependency] = [
-    .init(
-        category: .essential,
-        package: .package(url: "https://github.com/OpenZesame/zesame.git", revision: "8918ddb06807724383ad2965461fffeea91f89af"), // branch: structured_concurrency
-        product: .product(name: "Zesame", package: "zesame"),
-        rationale: "Zilliqa Swift SDK, containing all account logic."
-    ),
-    
-    .init(
-        category: .convenience,
-        package: .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.2"),
-        product: "KeychainAccess",
-        rationale: "Keychain is very low level"
-    ),
-    
-    .init(
-        category: .navigation,
-        package: .package(url: "https://github.com/rundfunk47/stinsen", from: "2.0.7"),
-        product: .product(name: "Stinsen", package: "stinsen"),
-        rationale: "Coordinator pattern"
-    ),
-    
-//    .init(
-//        category: .navigation,
-//        package: .package(url: "https://github.com/matteopuc/swiftui-navigation-stack", from: "1.0.4"),
-//        product: "NavigationStack",
-//        rationale: "Manual control over navigation stack"
-//    )
-    
-//    .init(
-//        category: .navigation,
-//        package: .package(url: "https://github.com/pointfreeco/swiftui-navigation", branch: "main"),
-//        product: "SwiftUINavigation",
-//        rationale: "Routing and IfLet Switch, CaseLet convenience"
-//    )
+	.init(
+		category: .essential,
+		// branch: structured_concurrency
+		package: .package(url: "https://github.com/OpenZesame/zesame.git", revision: "8918ddb06807724383ad2965461fffeea91f89af"),
+		product: .product(name: "Zesame", package: "zesame"),
+		rationale: "Zilliqa Swift SDK, containing all account logic."
+	),
+	
+	.init(
+		category: .architecture,
+		package: .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "0.33.1"),
+		product: .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+		rationale: "Testable, modular, scalable architecture gaining grounds as the go-to architecture for SwiftUI."
+	),
 
-//    .init(
-//        category: .navigation,
-//        package: .package(url: "https://github.com/johnpatrickmorgan/FlowStacks", from: "0.1.0"),
-//        product: "FlowStacks",
-//        rationale: "Coordinator pattern and routing"
-//    )
-    
-        .init(
-            category: .view,
-            package: .package(url: "https://github.com/EFPrefix/EFQRCode.git", from: "6.2.0"),
-            product: "EFQRCode",
-            rationale: "Convenient QR code generator supporting macOS and iOS."
-        ),
-  
-        // Alterarnative QR code scanners:
-        // * https://github.com/mercari/QRScanner (no direct SwiftUI support?)
-        // * https://github.com/yannickl/QRCodeReader.swift (no direct SwiftUI support?)
-        .init(
-            category: .view,
-            package: .package(url: "https://github.com/twostraws/CodeScanner.git", from: "2.1.1"),
-            product: "CodeScanner",
-            rationale: "Convenient QR code scanning view."
-        )
+	.init(
+		category: .convenience,
+		package: .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.2"),
+		product: "KeychainAccess",
+		rationale: "Keychain is very low level"
+	),
+
+	.init(
+		category: .navigation,
+		package: .package(url: "https://github.com/rundfunk47/stinsen", from: "2.0.7"),
+		product: .product(name: "Stinsen", package: "stinsen"),
+		rationale: "Coordinator pattern",
+		alternatives: [
+			.init(url: "https://github.com/matteopuc/swiftui-navigation-stack", abstract: "Manual control over navigation stack"),
+			.init(url: "https://github.com/pointfreeco/swiftui-navigation", abstract: "Routing and IfLet Switch, CaseLet convenience"),
+			.init(url: "https://github.com/johnpatrickmorgan/FlowStacks", abstract: "Coordinator pattern and routing")
+		]
+	),
+
+	.init(
+		category: .view,
+		package: .package(url: "https://github.com/EFPrefix/EFQRCode.git", from: "6.2.0"),
+		product: "EFQRCode",
+		rationale: "Convenient QR code generator supporting macOS and iOS."
+	),
+
+	.init(
+		category: .view,
+		package: .package(url: "https://github.com/twostraws/CodeScanner.git", from: "2.1.1"),
+		product: .product(name: "CodeScanner", package: "CodeScanner", condition: .when(platforms: [.iOS])),
+		rationale: "Convenient QR code scanning view.",
+		alternatives: [
+			.init(url: "https://github.com/mercari/QRScanner", abstract: "Lacks SwiftUI support?"),
+			.init(url: "https://github.com/yannickl/QRCodeReader.swift", abstract: "Lacks SwiftUI support?")
+		]
+	)
 ]
 
 let package = Package(
-    name: "ZhipEngine",
-    platforms: [.macOS(.v12), .iOS(.v15)],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "ZhipEngine",
-            targets: ["ZhipEngine"]),
-    ],
-    dependencies: dependencies.packageDependencies,
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "ZhipEngine",
-            dependencies: dependencies.targetDependencies),
-        .testTarget(
-            name: "ZhipEngineTests",
-            dependencies: ["ZhipEngine"]),
-    ]
+	name: "ZhipEngine",
+	platforms: [.macOS(.v12), .iOS(.v15)],
+	products: [
+		// Products define the executables and libraries a package produces, and make them visible to other packages.
+		.library(
+			name: "ZhipEngine",
+			targets: ["ZhipEngine"]),
+	],
+	dependencies: dependencies.packageDependencies,
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages this package depends on.
+		.target(
+			name: "ZhipEngine",
+			dependencies: dependencies.targetDependencies
+		),
+		.testTarget(
+			name: "ZhipEngineTests",
+			dependencies: ["ZhipEngine"]),
+	]
 )
